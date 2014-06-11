c1cfd1ac96f8ee494b577d8938f29b14ab8ae580
MSzweda <malgorzata.szweda@netguru.pl>
2014-06-11 17:43:04 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aaewebinar::Application.config.secret_key_base = '7ad9b0bb4654f43bf48decc4304a7558b8577f5e58128c783f1689060924fd9aba7b3833089b4f583d5b7dc9e5e2504291b5e171d7f77c2eee35301efe250396'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Aaewebinar::Application.config.session_store :cookie_store, key: '_aaewebinar_session'
